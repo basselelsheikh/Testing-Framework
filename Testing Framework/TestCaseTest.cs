3f5ef41ae18e9887f179d6f4d5fc7fb37244a55d
Bassel Elsheikh <bassel.elsheikh@gmail.com>
2023-06-06 01:58:32 +0300

    Added SetUp and Run

---
+++ Testing Framework/TestCaseTest.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Testing_Framework
+{
+    internal class TestCaseTest : TestCase
+    {
+        public TestCaseTest(string name) : base(name)
+        { }
+        public void TestRunning()
+        {
+            var test = new WasRun("TestMethod");
+            Console.WriteLine(test.wasRun);
+            test.run();
+            Console.WriteLine(test.wasRun);
+        }
+        public void TestSetUp()
+        {
+            var test = new WasRun("TestMethod");
+            test.run();
+            if (test.WasSetUp) Console.WriteLine("PASS");
+            else Console.WriteLine("FAIL");
+        }
+    }
+}
